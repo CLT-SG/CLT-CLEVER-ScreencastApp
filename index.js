40abeaf39fc406d9f61ec18ee9aa02b205b27d3c
Fahmi-CLT <fahmi@closed-loop.biz>
2020-06-18 12:42:47 +0800

    remove clever-config

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ const fs = require('fs')
 const os = require('os')
 const homedir = os.homedir()
 const isReachable = require('is-reachable')
-const appdir = path.normalize(homedir + '/app')
 const logdir = path.normalize(homedir + '/clevervnc-log')
 const iconPath = path.join(__dirname, '/src/assets/media/logo.png')
 const publishPath = path.join(__dirname, '/src/assets/media/publish.png')
@@ -23,14 +22,6 @@ var log = require('electron-log')
 log.transports.file.file = logdir + '/' + datelog + '.log'
 var pingstat
 
-//check if folder app and logs exists
-if (!fs.existsSync(appdir)) {
-  fs.mkdir(appdir, 0755, (err) => {
-    if (err) {
-      log.warn(err)
-    }
-  })
-}
 if (!fs.existsSync(logdir)) {
   fs.mkdir(logdir, 0755, (err) => {
     if (err) {
@@ -80,56 +71,6 @@ const template = [
 const gotTheLock = app.requestSingleInstanceLock()
 
 try {
-  //update config-app.js to current update
-  fs.stat(appdir + '/clever-config.js', function (err, stats) {
-    if (err) {
-      log.warn(err)
-    } else {
-      var mtime = stats.mtime
-      mtime = date.format(mtime, 'YYYY-MM-DD')
-      var updateDate = date.parse('2020-02-06', 'YYYY-MM-DD')
-      updateDate = date.format(updateDate, 'YYYY-MM-DD')
-      if (mtime < updateDate) {
-        fs.writeFile(appdir + '/clever-config.js',
-          "var hostserver = '192.168.1.88'; // change hostname here\r\n\r\n\r\n" +
-          "module.exports.hostserver = hostserver; // localhost in local network\r\n" +
-          "module.exports.cleverweb = hostserver + ':3000'; // localhost:3000 in local network\r\n" +
-          "module.exports.mediaserver = hostserver + ':8000'; // localhost:8000 in local network\r\n" +
-          "module.exports.screenapi = hostserver + ':9559'; // localhost:9559 in local network\r\n" +
-          "module.exports.screenserver = hostserver + ':7890'; // localhost:7890 in local network\r\n" +
-          "module.exports.timeout = 10000;\r\n",
-          function (err, data) {
-            if (err) {
-              log.warn(err)
-            }
-            log.info('Config file created.')
-            const options = {
-              type: 'info',
-              buttons: ['Ok'],
-              defaultId: 2,
-              title: 'Setup and configuration',
-              message: 'Config file has been updated.',
-              detail: 'Please change hostserver at this location ' + appdir + '/clever-config.js \r\n' +
-                '\r\n\r\n' +
-                'Copyright © 2000-' + date.format(now, 'YYYY') + ' by Closed-loop Technology Pte Ltd. All rights reserved \r\n' +
-                ' www.closed-loop.biz'
-            }
-            dialog.showMessageBox(null, options).then((data) => {
-              if (data.response == 0) {
-                app.exit()
-                app.relaunch()
-              }
-            })
-          })
-      }
-    }
-  })
-} catch (err) {
-  console.log(err)
-  log.warn(err)
-}
-try {
-  const config = require(appdir + '/clever-config')
   if (!gotTheLock) {
     app.exit()
   } else {
@@ -275,35 +216,4 @@ try {
   }
 } catch (ex) {
   log.warn(ex)
-  fs.writeFile(appdir + '/clever-config.js',
-    "var hostserver = '192.168.1.88'; // change hostname here\r\n\r\n\r\n" +
-    "module.exports.hostserver = hostserver; // localhost in office\r\n" +
-    "module.exports.cleverweb = hostserver + ':3000'; // localhost:3000 in local network\r\n" +
-    "module.exports.mediaserver = hostserver + ':8000'; // localhost:8000 in local network\r\n" +
-    "module.exports.screenapi = hostserver + ':9559'; // localhost:9559 in local network\r\n" +
-    "module.exports.screenserver = hostserver + ':7890'; // localhost:7890 in local network\r\n" +
-    "module.exports.timeout = 10000;\r\n",
-    function (err, data) {
-      if (err) {
-        log.warn(err)
-      }
-      log.info('Config file created.')
-      const options = {
-        type: 'info',
-        buttons: ['Ok'],
-        defaultId: 2,
-        title: 'Setup and configuration',
-        message: 'Config file has been created.',
-        detail: 'Please change hostserver at this location ' + appdir + '/clever-config.js \r\n' +
-          '\r\n\r\n' +
-          'Copyright © 2000-' + date.format(now, 'YYYY') + ' by Closed-loop Technology Pte Ltd. All rights reserved \r\n' +
-          ' www.closed-loop.biz'
-      };
-      dialog.showMessageBox(null, options).then((data) => {
-        if (data.response == 0) {
-          app.exit()
-          app.relaunch()
-        }
-      })
-    })
 }
