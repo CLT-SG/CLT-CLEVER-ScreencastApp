cffc48156bd2a8ba017c107901f9f9fd19a19d42
Fahmi-CLT <fahmi@closed-loop.biz>
2020-11-12 15:21:54 +0800

    disable devtools

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ try {
         webPreferences: {
           webSecurity: false,
           enableRemoteModule: true,
-          devTools: true,
+          devTools: false,
           nodeIntegration: true,
           webSecurity: false,
           zoomFactor: 1
