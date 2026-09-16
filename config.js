1814802bd3f0e76ef4b782c6d5687d632ebbef2b
giegiey23 <fahmi.kkpg@gmail.com>
2026-09-16 10:14:22 +0800

    Update audio settings and UI for WebRTC integration

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 exports.pcname = "DESKTOP"
 exports.autorestart = 1800000
 exports.autostartup = true
-exports.audio = false
-exports.systemAudio = false
+exports.audio = true
+exports.systemAudio = true
 exports.microphone = false
 exports.speakerOutput = true
 exports.twoWayAudio = false
@@ -11,7 +11,7 @@ exports.autoshare = true
 // Window configuration
 exports.window = {
   width: 1180,         // Default window width in pixels
-  height: 760,         // Default window height in pixels
+  height: 900,         // Default window height in pixels
   minWidth: 1024,      // Minimum window width
   minHeight: 700,      // Minimum window height
   resizable: true      // Whether window can be resized
